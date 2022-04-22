7f1cb5703581112f450f796880f98e95fae90e76
Albert Albala <albert@mediatribe.net>
2022-04-22 07:48:57 -0400

    Better separate installation of extension for basic setup (#10)

---
+++ docker-resources/load-extensions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Install extensions.
+#
+set -e
+
+# This is how to install an extension; we are using TinyMCE as an example,
+# use this as a template to install your own extensions in your fork or
+# copy of this project.
+
+# https://www.mediawiki.org/wiki/Extension:TinyMCE#Installation
+# echo "Downloading and installing TinyMCE"
+# curl -O -L https://github.com/wikimedia/mediawiki-extensions-TinyMCE/archive/refs/heads/master.zip
+# unzip master.zip
+# rm master.zip
+# mv mediawiki-extensions-TinyMCE-master extensions/TinyMCE
