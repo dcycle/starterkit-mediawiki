d6f760d7fe9540a95a671acf4f13e6ad4046b71f
Albert Albala <albert@mediatribe.net>
2022-04-22 07:55:36 -0400

    Make sure all sample code can be uncommented without errors

--- docker-resources/load-extensions.sh
+++ docker-resources/load-extensions.sh
@@ -8,8 +8,8 @@ set -e
 # use this as a template to install your own extensions in your fork or
 # copy of this project.
 
-# https://www.mediawiki.org/wiki/Extension:TinyMCE#Installation
 # echo "Downloading and installing TinyMCE"
+# echo "See https://www.mediawiki.org/wiki/Extension:TinyMCE#Installation"
 # curl -O -L https://github.com/wikimedia/mediawiki-extensions-TinyMCE/archive/refs/heads/master.zip
 # unzip master.zip
 # rm master.zip
